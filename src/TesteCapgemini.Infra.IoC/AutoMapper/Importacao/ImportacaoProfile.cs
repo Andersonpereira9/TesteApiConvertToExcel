c3576ce0c9b750caa3b469dcb24bffbef93e35cc
Anderson Silva <anderson.silva@dorconsultoria.com.br>
2020-05-23 19:22:34 -0300

    map importacao response

---
+++ src/TesteCapgemini.Infra.IoC/AutoMapper/Importacao/ImportacaoProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TesteCapgemini.Domain.Arguments;
+using TesteCapgemini.Domain.Entities;
+
+namespace TesteCapgemini.Infra.IoC.AutoMapper.Importacao
+{
+    public class ImportacaoProfile : Profile
+    {
+        public ImportacaoProfile()
+        {
+            CreateMap<ImportacaoModel, ImportacaoListaResponse>()
+              .ForMember(d => d.Id, o => o.MapFrom(m => m.Id))
+              .ForMember(d => d.DataImportacao, o => o.MapFrom(m => m.DataImportacao))
+              //.ForMember(d => d.MenorData, o => o.MapFrom(m => m.Pedidos.Select(s => s.DataEntrega).OrderBy
+              //.ForMember(d => d.Quantidade, o => o.MapFrom(m => m.Quantidade))
+              ;
+        }
+    }
+}
